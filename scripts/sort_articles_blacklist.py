571d123d2131fafbb955125e18fde3ef4dc553fd
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2018-12-27 23:06:50 -0500

    Drop duplicates

--- scripts/sort_articles_blacklist.py
+++ scripts/sort_articles_blacklist.py
@@ -6,4 +6,5 @@ _PATH = CONFIG_DIR / 'articles_blacklist.csv'
 
 df = pd.read_csv(_PATH, dtype={'URL_ID': str, 'Category': 'category'})
 df.sort_values('URL_ID', ascending=False, inplace=True)
+df.drop_duplicates('URL_ID', inplace=True)
 df.to_csv(_PATH, index=False)
