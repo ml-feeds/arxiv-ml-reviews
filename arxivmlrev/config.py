2f1eddce4bc26d344197f1bae802f095881dcae5
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2018-12-28 16:28:27 -0500

    Update delay between queries

--- arxivmlrev/config.py
+++ arxivmlrev/config.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 from typing import Set
 
@@ -12,8 +13,8 @@ def _textfile_list(path: Path) -> Set[str]:
 
 
 CATEGORIES = _textfile_list(Path(CONFIG_DIR) / 'categories.txt')
-DELAY_BETWEEN_QUERIES = 8  # Per recommendation of 3 at https://arxiv.org/help/api/user-manual#paging
 MAX_RESULTS_PER_QUERY = 400
+DELAY_BETWEEN_QUERIES = min(3, math.ceil(math.log(MAX_RESULTS_PER_QUERY)))
 TERMS = _textfile_list(Path(CONFIG_DIR) / 'terms.txt')  # Lowercase phrases without punctuation.
 TERMS_BLACKLIST = _textfile_list(Path(CONFIG_DIR) / 'terms_blacklist.txt')
 URL_ID_BLACKLIST = set(pd.read_csv(CONFIG_DIR / 'articles_blacklist.csv', dtype={'URL_ID': str},
