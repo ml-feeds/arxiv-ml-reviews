1181f2773139d781ca8562b4dce2ad0b183b8469
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2018-12-27 22:54:54 -0500

    Sort articles blacklist

---
+++ scripts/sort_articles_blacklist.py
@@ -0,0 +1,9 @@
+from arxivmlrev.config import CONFIG_DIR
+
+import pandas as pd
+
+_PATH = CONFIG_DIR / 'articles_blacklist.csv'
+
+df = pd.read_csv(_PATH, dtype={'URL_ID': str, 'Category': 'category'})
+df.sort_values('URL_ID', ascending=False, inplace=True)
+df.to_csv(_PATH, index=False)
--- scripts/sort_blacklist.py
+++
@@ -1,7 +0,0 @@
-from arxivmlrev.config import DATA_DIR
-
-import pandas as pd
-
-df = pd.read_csv(DATA_DIR / 'blacklist.csv', dtype={'Category': 'category'})
-df.sort_values('URL_ID', ascending=False, inplace=True)
-df.to_csv(DATA_DIR / 'blacklist.csv', index=False)
