11f659207f88104481f2548fa2d54cff23835c54
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2018-12-29 14:22:25 -0500

    Refactor

---
+++ scripts/refresh_config_articles.py
@@ -0,0 +1,19 @@
+import arxiv
+
+from arxivmlrev import config
+from arxivmlrev.result import Result
+
+df = config.CONFIG_ARTICLES.copy()
+url_ids = df['URL_ID'].tolist()
+assert len(url_ids) <= 2000
+results = arxiv.query(id_list=url_ids, max_results=len(url_ids), sort_by='submittedDate')
+assert len(results) == len(url_ids)
+
+for result in results:
+    result = Result(result)
+    df.loc[df.URL_ID == result.url_id, ['Category', 'Title']] = result.category, result.title
+
+
+df = df.sort_values(['State', 'URL_ID'], ascending=False).drop_duplicates('URL_ID')
+if not df.equals(config.CONFIG_ARTICLES):
+    df.to_csv(config.CONFIG_ARTICLES_PATH, index=False)
