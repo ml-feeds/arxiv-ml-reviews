acb534900111c19c6ce8fb89804e266ad545f3d5
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2018-12-29 14:07:08 -0500

    Refactor

--- arxivmlrev/config.py
+++ arxivmlrev/config.py
@@ -4,19 +4,21 @@ from typing import Set
 
 import pandas as pd
 
-CONFIG_DIR = Path(__file__).parent / '_config'
-DATA_DIR = Path(__file__).parents[1] / 'data'
 
-
-def _textfile_list(path: Path) -> Set[str]:
+def _textfile_set(path: Path) -> Set[str]:
     return set(path.read_text().strip().split('\n'))
 
 
-ARTICLES_PATH = DATA_DIR / 'articles.csv'
-CATEGORIES = _textfile_list(Path(CONFIG_DIR) / 'categories.txt')
+CONFIG_DIR = Path(__file__).parent / '_config'
+DATA_DIR = Path(__file__).parents[1] / 'data'
+
+CATEGORIES = _textfile_set(Path(CONFIG_DIR) / 'categories.txt')
+CONFIG_ARTICLES = pd.read_csv(CONFIG_DIR / 'articles.csv', dtype={'URL_ID': str})
+DATA_ARTICLES_COLUMNS = 'URL_ID', 'Category', 'Title', 'Year_Published', 'Year_Updated'
+DATA_ARTICLES_PATH = DATA_DIR / 'articles.csv'
 MAX_RESULTS_PER_QUERY = 400
 DELAY_BETWEEN_QUERIES = min(3, math.ceil(math.log(MAX_RESULTS_PER_QUERY)))
-TERMS = _textfile_list(Path(CONFIG_DIR) / 'terms.txt')  # Lowercase phrases without punctuation.
-TERMS_BLACKLIST = _textfile_list(Path(CONFIG_DIR) / 'terms_blacklist.txt')
-URL_ID_BLACKLIST = set(pd.read_csv(CONFIG_DIR / 'articles_blacklist.csv', dtype={'URL_ID': str},
-                                   usecols=['URL_ID'])['URL_ID'])
+TERMS = _textfile_set(Path(CONFIG_DIR) / 'terms.txt')  # Lowercase phrases without punctuation.
+TERMS_BLACKLIST = _textfile_set(Path(CONFIG_DIR) / 'terms_blacklist.txt')
+URL_ID_BLACKLIST = set(CONFIG_ARTICLES[CONFIG_ARTICLES['State'] == 0]['URL_ID'])
+URL_ID_WHITELIST = set(CONFIG_ARTICLES[CONFIG_ARTICLES['State'] == 1]['URL_ID'])
