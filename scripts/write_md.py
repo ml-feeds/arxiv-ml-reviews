c12e5f224ef020ee8e24b6ca781132cb94a58e8f
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2018-12-30 12:35:47 -0500

    Refresh list

--- scripts/write_md.py
+++ scripts/write_md.py
@@ -8,16 +8,17 @@ import pandas as pd
 categories = ', '.join(f'[{cat}](https://arxiv.org/list/{cat}/recent)' for cat in sorted(config.CATEGORIES))
 
 prologue = f"""
-This is a mostly auto-generated list of review articles on machine learning that are on arXiv.
-Although some of them were written for a specific technical audience or application, the techniques described are
-nonetheless generally relevant.
-The list is sorted reverse chronologically. It was generated on {date.today()}.
-It includes articles from these arXiv categories: {categories}\n
+This is a mostly auto-generated list of review articles on machine learning and artificial intelligence that are on \
+[arXiv](https://arxiv.org/). \
+Although some of them were written for a specific technical audience or application, the techniques described are \
+nonetheless generally relevant. \
+The list is sorted reverse chronologically. It was generated on {date.today()}. \
+It includes articles from these arXiv categories: {categories}
 """
 
 df = pd.read_csv(config.DATA_DIR / 'articles.csv', dtype={'URL_ID': str, 'Category': 'category'})
 with (config.DATA_DIR / 'articles.md').open('w') as md:
-    md.write(f'# Review articles\n{prologue}')
+    md.write(f'# Review articles\n{prologue}\n')
     for _, row in df.iterrows():
         years = row.Year_Published if (row.Year_Published == row.Year_Updated) else \
             f'{row.Year_Published}-{row.Year_Updated}'
