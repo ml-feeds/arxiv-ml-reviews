6055230098f1a8724fb867e79773f96a49e30886
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-01-05 22:25:56 -0500

    Add humanized_rss function

--- arxivmlrev/util/resource.py
+++ arxivmlrev/util/resource.py
@@ -1,14 +1,21 @@
 from pathlib import Path
 from resource import getpagesize
 
+from arxivmlrev.util.humanize import humanize_bytes
+
+
 _PAGESIZE = getpagesize()
 _PATH = Path('/proc/self/statm')
 
 
-def get_resident_set_size() -> int:
+def resident_set_size() -> int:
     """Return the current resident set size in bytes."""
     # Ref: https://stackoverflow.com/a/53486808/
     # statm columns are: size resident shared text lib data dt
     statm = _PATH.read_text()
     fields = statm.split()
     return int(fields[1]) * _PAGESIZE
+
+
+def humanized_rss() -> str:
+    return humanize_bytes(resident_set_size())
