f65f97bffeb5ce45213f848cfea002fe10372523
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-09-28 12:51:41 -0400

    Include link to Issues page

--- arxivmlrev/results.py
+++ arxivmlrev/results.py
@@ -71,6 +71,8 @@ class Results:
         The [source code](https://github.com/ml-feeds/arxiv-ml-reviews) along with \
         [raw data](https://raw.githubusercontent.com/ml-feeds/arxiv-ml-reviews/master/data/articles.csv) for \
         generating this page are linked. \
+        Any incorrect or missing entries can be reported as an \
+        [issue](https://github.com/ml-feeds/arxiv-ml-reviews/issues). \
         This page is currently not automatically updated. \
         An auto-updated [RSS feed](https://us-east1-ml-feeds.cloudfunctions.net/arxiv-ml-reviews) is available.
         """.strip()
